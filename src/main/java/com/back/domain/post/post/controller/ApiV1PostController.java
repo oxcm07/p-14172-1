62e2cba5ee18d9b2ebbb3c4e755a30f708980def
oxcm07 <oxcm0727@gmail.com>
2026-05-20 12:50:34 +0900

    016

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,7 +3,9 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.ForPostRsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +36,19 @@ public class ApiV1PostController {
         Post post = postService.findById(id).get();
 
         return new PostDto(post);
+    }
+
+    @GetMapping("/{id}/delete")
+    @Transactional
+    public ForPostRsData delete(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+
+        postService.delete(post);
+
+        return new ForPostRsData(
+                "200-1",
+                "%d번 글이 삭제되었습니다.".formatted(id),
+                new PostDto(post)
+        );
     }
 }
