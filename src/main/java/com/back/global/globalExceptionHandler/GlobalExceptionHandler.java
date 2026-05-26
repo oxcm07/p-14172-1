cf2238b0f7aef3cb07224db54eadbd6f88b4a1b9
oxcm07 <oxcm0727@gmail.com>
2026-05-26 12:29:18 +0900

    048

---
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.back.global.globalExceptionHandler;
+
+import com.back.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(NOT_FOUND) // 상태 코드를 지정하기 위해 필요
+    public RsData<Void> handle() {
+        return new RsData<>(
+                "404-1",
+                "해당 데이터가 존재하지 않습니다."
+        );
+    }
+}
--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -131,6 +131,23 @@ public class ApiV1PostControllerTest {
     }
 
     @Test
+    @DisplayName("글 단건조회, 404")
+    void t6() throws Exception {
+        int id = Integer.MAX_VALUE;
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/posts/" + id)
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     @DisplayName("글 다건조회")
     void t5() throws Exception {
         ResultActions resultActions = mvc
@@ -139,12 +156,13 @@ public class ApiV1PostControllerTest {
                 )
                 .andDo(print());
 
+        List<Post> posts = postService.findAll();
+
         resultActions
                 .andExpect(handler().handlerType(ApiV1PostController.class))
                 .andExpect(handler().methodName("getItems"))
-                .andExpect(status().isOk());
-
-        List<Post> posts = postService.findAll();
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(posts.size()));
 
         for (int i = 0; i < posts.size(); i++) {
             Post post = posts.get(i);
